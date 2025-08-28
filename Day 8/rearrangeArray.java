75ea29559d37c0bf1a1c390fb6a86719c26c93d0
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2025-08-28 18:49:22 +0530

    Create rearrangeArray.java

---
+++ Day 8/rearrangeArray.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int posIndex = 0, negIndex = 1;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            }else{
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+}
